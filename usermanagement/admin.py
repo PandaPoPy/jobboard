1d49718fd41c3deb7e3622afdf83bb7376a9e95a
Vincent <vlhoste@thinkcode.co>
2017-01-20 16:07:14 +0100

    Create Admin Models

--- usermanagement/admin.py
+++ usermanagement/admin.py
@@ -1,2 +1,12 @@
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib import admin
 
+from .models import CandidateUser, EnterpriseUser, Offer, Enterprise, Appliance
+
+
+admin.site.register(CandidateUser)
+admin.site.register(EnterpriseUser)
+admin.site.register(Offer)
+admin.site.register(Enterprise)
+admin.site.register(Appliance)
 
